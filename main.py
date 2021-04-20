784cb1d662552b7ef87aa4dadbd9968aaa94cd57
Kulazar <eneanuch@yandex.ru>
2021-04-20 17:31:11 +0300

    add error page

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ def main_page():
     return render_template("main_page.html")
 
 
+@app.errorhandler(400)
+@app.errorhandler(404)
+def handle_bad_request(e):
+    return render_template("error.html", error=e.name)
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
