b010eddb86acf63e89036544bfe752894deed1e7
Kulazar <eneanuch@yandex.ru>
2021-04-20 16:56:56 +0300

    many new

--- import_all.py
+++ import_all.py
@@ -1,4 +1,1 @@
 # there are all imports
-
-from managers import FuncManager, DBManager, TranslateManager
-from flask import url_for, Flask
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import import_all
+from managers import FuncManager, DBManager, TranslateManager
+from flask import url_for, Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/main")
+def main_page():
+    return render_template("main_page.html")
 
 
 if __name__ == '__main__':
-    pass
+    app.run(port=8080, host='127.0.0.1')
